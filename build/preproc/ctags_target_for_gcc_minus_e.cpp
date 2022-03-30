e5250a5ffb7999ec90ef1bfb2278ba9258bfcd97
ik9702 <54349030+ik9702@users.noreply.github.com>
2022-03-30 14:48:13 +0900

    수업 끝

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -3,69 +3,19 @@ void setup(){
   Serial.begin(9600);
   pinMode(3, 0x1);
 }
-int val = 0;
-int cnt = 10;
-int bright = 100;
-float target = 1400;
-int step = 5;
-
+int i = random(1, 101);
 void loop(){
-  Serial.print(target);
-  Serial.print("         ");
-  int sum = 0;
-  for(int i=0;i<=cnt;i++){
-    val = analogRead(A0);
-    sum = sum+val;
-    delay(10);
-  }
-  val = sum/cnt;
+  do{
+    digitalWrite(3, 1);
+    Serial.print(i);
+    Serial.println(" on");
 
-  float vol = 5*val/1023.0;
-  float ohm = 10000*vol/(5-vol);
-  Serial.print(ohm);
-
-
-  if(bright>=0&&bright<255&&ohm<target){
-    bright = bright-step;
-  }
-  else if(bright<255&&ohm>target){
-    bright = bright+step;
-  }
-  else if(bright<0){
-    bright = bright+step;
-  }
-  else{
-    bright = bright-step;
-  }
-  Serial.print("      ");
-  Serial.println(bright);
-  analogWrite(3, bright);
+    i = random(1,101);
+    delay(100);
+  }while(i<90);
+  digitalWrite(3,0);
+  Serial.print(i);
+  Serial.println("off");
+  i = random(1,101);
+  delay(100);
 }
-
-// void setup(){
-//   Serial.begin(9600);
-//   pinMode(3, OUTPUT);
-// }
-// int val = 0;
-// int cnt = 50;
-// void loop(){
-//   int sum = 0;
-//   for(int i=0;i<=cnt;i++){
-//     val = analogRead(A0);
-//     sum = sum+val;
-//     delay(10);
-//   }
-//   val = sum/cnt;
-//   float vol = 5*val/1023.0;
-//   float ohm = 10000*vol/(5-vol);
-//   float cal = 1.6;
-//   float hmm = pow(ohm, cal);
-//   float lux = 1.5/hmm;
-//   Serial.print(val);//0-1023
-//   Serial.print("    ");
-//   Serial.print(ohm);
-//   Serial.print("    ");
-//   Serial.println(lux);
-
-
-// }
